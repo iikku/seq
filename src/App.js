f99c2c05d91c592e62cef4dff9d46cb8af6b6b9d
Iikku Mattila <iikku.mattila@vincit.com>
2019-05-30 09:13:27 +0300

    Add webmidi and play a few notes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import WebMidi from 'webmidi';
+import lifecycle from 'react-pure-lifecycle';
 import logo from './logo.svg';
 import './App.css';
 
+const methods = {
+  componentDidMount(props) {
+    WebMidi.enable(function (err) {
+
+      if (err) {
+        console.log("WebMidi could not be enabled.", err);
+        return;
+      }
+
+      const digitone = WebMidi.outputs[1];
+      digitone.playNote("C3", 1, {duration: 1000});
+      digitone.playNote("E3", 2, {duration: 1000});
+
+    });
+  }
+};
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          oof
         </p>
         <a
           className="App-link"
@@ -23,4 +43,4 @@ function App() {
   );
 }
 
-export default App;
+export default lifecycle(methods)(App);
